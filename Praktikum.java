6568c237127f16e1a13515e7909eda012c06feb1
muhammad-di <dzhabrailov3.muhammad@yandex.ru>
2022-11-27 04:29:40 +0300

    a simple program to determine is year a leap one or not

---
+++ Praktikum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println("12.09." + year);
+        } else {
+            System.out.println("13.09." + year);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 100 == 0) {
+            return false;
+        } else  {
+            return true;
+        }
+
+    }
+
+}
